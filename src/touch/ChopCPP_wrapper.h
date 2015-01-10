db60fba10f85cf69f44b49fa298e986a3c0c9742
t3kt <tommy.etkin@gmail.com>
2015-01-10 14:30:22 -0500

    added visual studio project

--- src/touch/ChopCPP_wrapper.h
+++ src/touch/ChopCPP_wrapper.h
@@ -9,9 +9,17 @@
 #ifndef threshold_ChopCPP_wrapper_h
 #define threshold_ChopCPP_wrapper_h
 
+#ifdef WIN32
+
+#include <Windows.h>
+
+#else
+
 #define _WIN64
 
 typedef void* HWND;
+
+#endif
 
 #include "CHOP_CPlusPlusBase.h"
 

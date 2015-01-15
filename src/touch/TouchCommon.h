cb4917e3db9fca29dc320ec9280ce36ceebe6148
t3kt <tommy.etkin@gmail.com>
2015-01-14 20:07:17 -0500

    attempting to optimize the input point handling a bit
    instead of copying everything into a vector, just use the original
    input channels

--- src/touch/TouchCommon.h
+++ src/touch/TouchCommon.h
@@ -14,7 +14,43 @@
 
 class CHOPInputPointSet : public PointSource {
 public:
+  CHOPInputPointSet(const CHOP_CHOPInput* inputs,
+                    int xI, int yI, int zI)
+  : _inputs(inputs)
+  , _xInputIndex(xI), _yInputIndex(yI) , _zInputIndex(zI) { }
   
+  int size() const override { return _inputs->length; }
+  ThreshPoint operator[](int i) const override;
+  ThVec3f getPosition(int i) const override;
+  //  ThColor getColor(int i) const override;
+  
+  //  int rInputIndex;
+  //  int gInputIndex;
+  //  int bInputIndex;
+  //  int aInputIndex;
+private:
+  int _xInputIndex;
+  int _yInputIndex;
+  int _zInputIndex;
+  const CHOP_CHOPInput* _inputs;
 };
+
+ThreshPoint CHOPInputPointSet::operator[](int i) const {
+  ThreshPoint point;
+  point.position.x = _inputs->channels[_xInputIndex][i];
+  point.position.y = _inputs->channels[_yInputIndex][i];
+  point.position.z = _inputs->channels[_zInputIndex][i];
+  //color ... probably going to remove that...?
+  return point;
+}
+
+ThVec3f CHOPInputPointSet::getPosition(int i) const {
+  ThVec3f position;
+  position.x = _inputs->channels[_xInputIndex][i];
+  position.y = _inputs->channels[_yInputIndex][i];
+  position.z = _inputs->channels[_zInputIndex][i];
+  return position;
+}
+
 
 #endif /* defined(__threshold__TouchCommon__) */

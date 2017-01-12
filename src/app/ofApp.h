fab8a048472c9b13e804a5673cadc061912bcfaa
t3kt <tommy.etkin@gmail.com>
2017-01-11 21:14:33 -0800

    removed empty method overrides

--- src/app/ofApp.h
+++ src/app/ofApp.h
@@ -24,19 +24,11 @@
 
 class ofApp : public ofBaseApp {
 public:
-  void setup();
-  void update();
-  void draw();
+  void setup() override;
+  void update() override;
+  void draw() override;
 
-  void keyPressed(int key);
-  void keyReleased(int key);
-  void mouseMoved(int x, int y );
-  void mouseDragged(int x, int y, int button);
-  void mousePressed(int x, int y, int button);
-  void mouseReleased(int x, int y, int button);
-  void windowResized(int w, int h);
-  void dragEvent(ofDragInfo dragInfo);
-  void gotMessage(ofMessage msg);
+  void keyPressed(int key) override;
   
 private:
   void dumpToLog() const;
